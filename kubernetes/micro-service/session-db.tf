1db091928c6f223e9359d5e4fe7a2c5f4e00dfe1
Aleonogwe Gideon <galosikhena@gmail.com>
2024-03-21 21:28:20 +0100

    added terrafomr script that automatically deploys the microservices and the databases

---
+++ kubernetes/micro-service/session-db.tf
@@ -0,0 +1,71 @@
+#---------------------------------
+# Kubernetes Deployment for DB
+#--------------------------------- 
+
+resource "kubernetes_deployment" "kube-session-db-deployment" {
+  metadata {
+    name      = "session-db"
+    namespace = kubernetes_namespace.kube-namespace.id
+    labels = {
+      name = "session-db"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        name = "session-db"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          name = "session-db"
+        }
+      }
+      spec {
+        container {
+          image = "redis:alpine"
+          name  = "session-db"
+
+      port {
+        name = "redis"
+        container_port = 6379
+      }
+
+      security_context {
+        capabilities {
+          drop = ["ALL"]
+          add = ["CHOWN", "SETGID", "SETUID"]
+        }
+        read_only_root_filesystem = false
+      }
+      }
+        node_selector = {
+        "beta.kubernetes.io/os" = "linux"
+      }
+    }
+  }
+}
+}
+
+resource "kubernetes_service" "kube-session-db-service" {
+  metadata {
+    name      = "cartsession-dbs"
+    namespace = kubernetes_namespace.kube-namespace.id
+
+    labels = {
+        name = "session-db"
+    }
+  }
+  spec {
+    selector = {
+      name = "session-db"
+    }
+    port {
+      port        = 6379
+      target_port = 6379
+    }
+  }
+}
