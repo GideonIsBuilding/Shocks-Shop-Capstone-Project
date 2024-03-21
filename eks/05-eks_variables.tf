740e481859c7fb7da5bc33a82d98d69b3ac9bb74
Aleonogwe Gideon <galosikhena@gmail.com>
2024-03-21 20:34:00 +0100

    added the terraform folder to provision EKS

---
+++ eks/05-eks_variables.tf
@@ -0,0 +1,22 @@
+variable "cluster_name" {
+  type        = string
+}
+
+variable "cluster_service_ipv4_cidr" {
+  type        = string
+}
+
+variable "cluster_version" {
+  type        = string
+}
+variable "cluster_endpoint_private_access" {
+  type        = bool
+}
+
+variable "cluster_endpoint_public_access" {
+  type        = bool
+}
+
+variable "cluster_endpoint_public_access_cidrs" {
+  type        = list(string)
+}
