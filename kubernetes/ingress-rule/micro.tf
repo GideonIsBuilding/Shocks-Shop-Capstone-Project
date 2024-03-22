6b0dc7662539e18cd55e42ae2bb9de2db790c349
Aleonogwe Gideon <galosikhena@gmail.com>
2024-03-22 18:12:15 +0100

    changed TLD of the domain name

--- kubernetes/ingress-rule/micro.tf
+++ kubernetes/ingress-rule/micro.tf
@@ -12,7 +12,7 @@ resource "kubernetes_ingress_v1" "micro-ingress" {
 
   spec {
     rule {
-      host = "gideon-aleonogwe.osikhena.com"
+      host = "gideon-aleonogwe.osikhena.xyz"
       http {
         path {
           backend {
