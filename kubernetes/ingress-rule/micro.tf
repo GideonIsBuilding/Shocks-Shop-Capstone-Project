de780fb37c5d4baf05bf30766ce01b97a18538bc
Aleonogwe Gideon <galosikhena@gmail.com>
2024-03-22 08:31:54 +0100

    changed the domain portion of the host name

--- kubernetes/ingress-rule/micro.tf
+++ kubernetes/ingress-rule/micro.tf
@@ -12,7 +12,7 @@ resource "kubernetes_ingress_v1" "micro-ingress" {
 
   spec {
     rule {
-      host = "gideon-aleonogwe.fotisia.com"
+      host = "gideon-aleonogwe.capstone.com"
       http {
         path {
           backend {
