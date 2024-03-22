6006a6ae94f5aac3d1d62180a1c33f1e63def08e
Aleonogwe Gideon <galosikhena@gmail.com>
2024-03-22 17:46:40 +0100

    commented the config block out and edited the variable name within the aws_route53.zone resource block

--- kubernetes/nginx-controller/null.tf
+++ kubernetes/nginx-controller/null.tf
@@ -25,7 +25,7 @@ resource "aws_route53_zone" "hosted_zone" {
 
 # Fetch the hosted zone ID for osikhena.com
 data "aws_route53_zone" "selected" {
-  name         = "osikhena.com."
+  name         = var.domain_name
   private_zone = false
 }
 
@@ -80,11 +80,11 @@ resource "acme_certificate" "certificate" {
     # guaranteed. You may want to explicitly configure them here if you
     # would like to use different credentials to those used by the main
     # Terraform provider
-    config = {
-      AWS_ACCESS_KEY_ID     = "${var.acme_challenge_aws_access_key_id}"
-      AWS_SECRET_ACCESS_KEY = "${var.acme_challenge_aws_secret_access_key}"
-      AWS_REGION            = "${var.acme_challenge_aws_region}"
-    }
+    # config = {
+    #   AWS_ACCESS_KEY_ID     = "${var.acme_challenge_aws_access_key_id}"
+    #   AWS_SECRET_ACCESS_KEY = "${var.acme_challenge_aws_secret_access_key}"
+    #   AWS_REGION            = "${var.acme_challenge_aws_region}"
+    # }
   }
 }
 
